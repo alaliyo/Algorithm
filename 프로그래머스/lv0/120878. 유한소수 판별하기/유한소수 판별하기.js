dbc739c95b111cfad2850d9f0e2d999799c7a96e
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-09 13:12:28 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,37 @@
+function solution(a, b) {
+    let aArr = [];
+    
+    while (a % 2 === 0) {
+        aArr.push(2);
+        a = a/2;
+    }
+    
+    for (let i = 3; i * i <= a; i = i + 2) {
+        while (a % i === 0) {
+            aArr.push(i);
+            a /= i;
+        }
+    }
+    
+    if (a > 2) {
+        aArr.push(a);
+    }
+    
+    for (let i = 0; i <= aArr.length; i++) {
+        if (b % aArr[i] === 0) {
+            b /= aArr[i];
+        }
+    }
+    
+    while (b % 2 === 0) {
+        b /= 2;
+    }
+    
+    while (b % 5 === 0) {
+        b /= 5;
+    }
+    
+    return b === 1 ? 1 : 2;
+}
+
+    
