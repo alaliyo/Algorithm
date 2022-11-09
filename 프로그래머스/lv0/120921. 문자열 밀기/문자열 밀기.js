54c78b989c525793964919a9659cce4b24b30bf2
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-09 11:13:06 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,19 +1,4 @@
 function solution(A, B) {
-    let count = 0;
-    let arr = [...B];
-    if (A === B) {
-        return count;
-    }
-    for (let i = 0; i < A.length; i++) {
-        arr.push(arr[0]);
-        arr.shift();
-        count += 1;
-        if (A === arr.join('')) {
-            break;
-        }
-    }
-    if (A !== arr.join('')) {
-        count = -1;
-    }
+    let count = (B+B).indexOf(A)
     return count;
 }
