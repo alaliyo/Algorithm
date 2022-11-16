e3ab089319e6196dc0d48d9cb2b6813485e3f4bb
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-16 09:44:31 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -2,10 +2,8 @@ function solution(id_pw, db) {
     for (let i = 0; i < db.length; i++) {
         if (id_pw[0] === db[i][0] && id_pw[1] === db[i][1]) {
             return "login";
-            break;
         } else if (id_pw[0] === db[i][0] && id_pw[1] !== db[i][1]) {
             return "wrong pw";
-            break;
         }
     }
     return "fail"
