aa67d026f45f7c4b22b355afce698d4836395edf
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-18 12:13:11 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,10 +1,8 @@
 function solution(n) {
-    let xNum = 0;
     for (let i = 1; i <= n; i++) {
-        xNum++;
         if (i % 3 === 0 || `${i}`.includes('3')) {
             n++;
         }
     } 
-    return xNum;
+    return n;
 }
