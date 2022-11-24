a5d884244b26df13c6795f3fb7484151b5c085dc
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-24 18:52:14 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,7 +1,10 @@
 function solution(quiz) {
-    for (let i = 0; i < quiz.length; i++) {
-        let arr = quiz[i].split(' = ');
-        new Function('return('+ arr[0] + ')')() === Number(arr[1]) ? quiz[i] = 'O' : quiz[i] = 'X'
-    }
-    return quiz
+    var answer = [];
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
 }
