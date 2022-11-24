c1afbbb6ba89efc8b102dc5dfb5df614765887a7
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-24 18:37:24 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -3,5 +3,4 @@ function solution(common) {
     const difference = (common[1] - common[0]);
     const distance = common[1] / common[0];
     return common[1] + difference === common[2] ? arrLast + difference : arrLast * distance;
-    
 }
