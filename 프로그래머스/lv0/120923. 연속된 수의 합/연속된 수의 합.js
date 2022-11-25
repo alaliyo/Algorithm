98b544a0b652b337905ab3c27508d2238ed9f523
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-25 18:52:37 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,11 +1,11 @@
 function solution(num, total) {
     let arr = [];
-    let middleNumber = Math.floor(total / num);
-    let subtracNumber = total % num === 0 ? Math.floor(num / 2) : num / 2 - 1;
-    let startNumber = middleNumber - subtracNumber;
+    let midNum = Math.floor(total / num);
+    let Number = total % num === 0 ? Math.floor(num / 2) : num / 2 - 1;
+    let start = midNum - Number;
 
     for (let i = 0; i < num; i++) {
-        arr.push(startNumber + i);
+        arr.push(start + i);
     }
     return arr;
 }
