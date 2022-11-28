a6ef915d0a9a4e7c3a32ba31b8123b8bf65ce1a7
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-28 14:20:52 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,14 @@
+function solution(lines) {
+    let arr = []
+    for (let i = 0; i < lines.length; i++) {
+        let index = lines[i];
+        index.sort((a, b) => a - b);
+        while (index[0] < index[1]) {
+            arr.push(index[0]);
+            index[0]++;
+        }
+    }
+    let arrFilter = arr.filter((item, index) => arr.indexOf(item) !== index);
+    let arrSet = [...new Set(arrFilter)];
+    return arrSet.length;
+}
