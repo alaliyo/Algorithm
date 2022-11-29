85a2af841a48559ce00ade893ac8a6eca30dd463
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-29 12:54:52 +0900

    [level 0] Title: 평행, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.js
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -3,13 +3,9 @@ function solution(dots) {
     for (let i = 0; i < dots.length; i++) {
         let j = 3;
         while (j <= 3 && i < j) {
-            let y = dots[i][1] >= dots[j][1] ? dots[i][1] - dots[j][1] : dots[j][1] - dots[i][1];
-            let x = dots[i][0] >= dots[j][0] ? dots[i][0] - dots[j][0] : dots[j][0] - dots[i][0];
             arr.push( (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]) );
-            j--
+            j--;
         }
     }
-    let arrFilter = arr.filter((e, i) => arr.indexOf(e) !== i);
-    return arrFilter.length > 0 ? 1 : 0;
-
+    return arr.filter((e, i) => arr.indexOf(e) !== i).length > 0 ? 1 : 0;
 }
