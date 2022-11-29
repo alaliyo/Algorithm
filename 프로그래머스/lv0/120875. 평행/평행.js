2c6f607c74620716dc7a02e365e181428ed9530e
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-29 12:31:36 +0900

    [level 0] Title: 평행, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -0,0 +1,15 @@
+function solution(dots) {
+    let arr = [];
+    for (let i = 0; i < dots.length; i++) {
+        let j = 3;
+        while (j <= 3 && i < j) {
+            let y = dots[i][1] >= dots[j][1] ? dots[i][1] - dots[j][1] : dots[j][1] - dots[i][1];
+            let x = dots[i][0] >= dots[j][0] ? dots[i][0] - dots[j][0] : dots[j][0] - dots[i][0];
+            arr.push(y/x);
+            j--
+        }
+    }
+    let arrFilter = arr.filter((e, i) => arr.indexOf(e) !== i);
+    return arrFilter.length > 0 ? 1 : 0;
+
+}
