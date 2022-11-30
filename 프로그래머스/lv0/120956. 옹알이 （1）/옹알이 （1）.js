38bc93371660294797893b91c5e68e50a5354fcb
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-11-30 10:04:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,5 +1,6 @@
 function solution(babbling) {
     let count = 0;
+    
     babbling.map(e => {
         let a = e.replace('aya', ' ').replace('ye', ' ').replace('woo', ' ').replace('ma', ' ');
         for (let i = 0; i < 7; i++) {
