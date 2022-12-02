78eb708830d6737b97aad033f86b4dde58f0bca7
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-02 13:39:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,3 +1,3 @@
 function solution(N) {
-    return arr = N.toString().split('').reduce((a, b) => Number(a) + Number(b), 0);
+    return N.toString().split('').reduce((a, b) => Number(a) + Number(b), 0);
 }
