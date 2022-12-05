fe4ad6e2f908f1683348a8782a5e4a7ec02fdf2c
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-05 09:58:11 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const nums = {
+        zero : 0, one : 1, two : 2, three : 3, four : 4,
+        five : 5, six : 6, seven : 7, eight : 8, nine : 9,
+    }
+    let answer = new RegExp(Object.keys(nums).join('|'), 'g');
+    
+    return Number(numbers.replace(answer, key => nums[key]));
+}
