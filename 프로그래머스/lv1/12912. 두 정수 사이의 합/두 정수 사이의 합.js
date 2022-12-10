d14eb82331c4876e9b8ff1db8f337406f84084d6
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-10 21:15:39 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,9 +1,8 @@
 function solution(a, b) {
-    let num = 0;
-    let min = Math.min(a, b);
-    const max = Math.max(a, b);
-    for (let i = min; min <= max; min++) {
-        num += min;
+    const dif = Math.abs(a - b);
+    if (dif % 2 === 0) {
+        return (a + b) / 2 * (dif+1)
+    } else {
+        return (a + b) * ((dif + 1) / 2)
     }
-    return num;
 }
