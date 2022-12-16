4c62933a013a615ee6a01ca3508afdc0db48b49b
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-16 11:26:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,17 +1,10 @@
 function solution(left, right) {
-    let arr = [];
+    let num = 0;
+    
     while (left <= right) {
-        arr.push(left);
+        Number.isInteger(Math.sqrt(left)) ? num -= left : num += left;
         left++
     }
 
-    for (let i = 0; i < arr.length; i++) {
-        let arrNum = [];
-        for (let j = 0; j <= arr[i]; j++) {
-            arr[i] % j === 0 ? arrNum.push(j) : null
-        }
-        arrNum.length % 2 !== 0 ? arr[i] *= -1 : null
-    }
-
-    return arr.reduce((f, e) => f + e , 0);
+    return num;
 }
