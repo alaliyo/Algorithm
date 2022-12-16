5541f91a5b836b64b3c556eda5e76d7665ff605f
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-16 11:27:38 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,10 +1,8 @@
 function solution(left, right) {
     let num = 0;
-    
     while (left <= right) {
         Number.isInteger(Math.sqrt(left)) ? num -= left : num += left;
-        left++
+        left++;
     }
-
     return num;
 }
