4d140bc8188227c8bf1f201b546e77b8c6f5be03
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-18 19:44:06 +0900

    [level 1] Title: 행렬의 덧셈, Time: 7.86 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,3 +1,5 @@
 function solution(arr1, arr2) {
-    return arr1.map((a,i) => a.map((b, j) => b + arr2[i][j]));
+    const answer = [];
+    arr1.forEach((arr, i) => answer.push(arr.map((v, j) => v + arr2[i][j])));
+    return answer;
 }
