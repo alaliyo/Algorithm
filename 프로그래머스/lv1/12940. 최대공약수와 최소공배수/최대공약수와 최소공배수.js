9bf067a06b00fb29f296d02b263330ba29800179
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-19 14:20:57 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,19 +1,6 @@
 function solution(n, m) {
-    let gcd = 1;
-    let lcm = 1;
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    const lcm = (a, b) => a * b / gcd(a, b);
     
-    for (let i = 2; i <= Math.min(n, m); i++) {
-        if (n % i === 0 && m % i === 0) {
-            gcd = i;
-        }
-    }
-    
-    while (true) {
-        if (lcm % n === 0 && lcm % m === 0) {
-            break;
-        }
-        lcm++;
-    }
-    
-    return [gcd, lcm]
+    return [gcd(n, m), lcm(n, m)];
 }
