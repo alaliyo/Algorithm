eea3f19775883e717c171f690cb5f50bc6806c1e
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-19 22:32:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,4 +1,7 @@
 function solution(my_string) {
-    const answer = my_string.split('').reverse().join('');
-    return answer;
+    let str = '';
+    for (let i = my_string.length - 1; i >= 0; i--) {
+        str += my_string[i];
+    }
+    return str;
 }
