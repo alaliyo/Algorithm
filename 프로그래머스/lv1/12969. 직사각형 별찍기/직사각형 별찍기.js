818f18def7669b86998d35b6cf49f645afa6b092
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-19 09:56:53 +0900

    [level 1] Title: 직사각형 별찍기, Time: 48.50 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,9 +2,12 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    const row = '*'.repeat(a)
-    for(let i =0; i < b; i++){
-        console.log(row)
+    let str = '';
+    for (let i = 0; i < b; i++) {
+        for (let j = 0; j < a; j++) {
+            str += '*'
+        }
+        str += '\n'
     }
-
+    console.log(str)
 });
