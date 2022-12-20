17cf4883e8bda3d68a4d76f08877b3adcb6b6d0e
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-20 14:21:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.73 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,9 @@
 function solution(arr) {
-    return arr.filter((e, i) => e !== arr[i + 1]);
+    let numArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            numArr.push(arr[i]);
+        }
+    }
+    return numArr;
 }
