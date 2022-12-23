3724913b2fc0f4f1e6832303b086e6155204f852
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-23 11:00:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,9 @@
+function solution(n, arr1, arr2) {
+    let arr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        arr1[i] = arr1[i].toString(2).padStart(n, '0');
+        arr2[i] = arr2[i].toString(2).padStart(n, '0');;
+    }
+    arr1.map((e, i) => arr.push(e.split('').map((a, j) => a === '1' || arr2[i][j] === '1' ? a = '1' : a = 0).join('').replace(/1/g, '#').replace(/0/g, ' ')));
+    return arr;
+}
