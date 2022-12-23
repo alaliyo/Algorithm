ca6ee598e6e7b40b288e96487e25734d6b283a6c
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-23 11:19:58 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,9 +1,5 @@
 function solution(array, commands) {
-    let numArr = [];
-    for (let i = 0; i < commands.length; i++) {
-        let e = array.slice(commands[i][0] -1, commands[i][1]).sort((a, b) => a - b);
-        numArr.push(e[commands[i][2] -1]);
-    }
+    commands.map((e, i) => commands[i] = array.slice(e[0] -1, e[1]).sort((a, b) => a - b)[e[2] -1]);
 
-    return numArr;
+    return commands;
 }
