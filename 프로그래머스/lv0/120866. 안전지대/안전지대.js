b7f7a35946eecec257459f5af03302d7667c2a70
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-26 11:45:35 +0900

    [level 0] Title: 안전지대, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,19 +1,41 @@
-function solution(b) {
-    const directions = [[0,0],[0,1],[0,-1],[1,1],[1,0],[1,-1],[-1,-1],[-1,0],[-1,1]]
-    let bombSet = new Set();
-
-    for(let i = 0; i < b.length; i++) {
-        for(let j = 0; j < b[i].length; j++) {
-            if(b[i][j] == 1) {
-                directions.forEach(el => {
-                    let [nextX, nextY] = el;
-                    [nextX, nextY] = [i+nextX, j+nextY];
-                    if(nextX >= 0 && nextX < b.length && nextY >= 0 && nextY < b[i].length) {
-                        bombSet.add(nextX+' '+nextY);
+function solution(board) {
+    let count = 0;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] % 2 !== 0) {
+                if (i - 1 >= 0) {
+                    if (j - 1 >= 0) {
+                        board[i-1][j-1] % 2 !== 0 ? board[i-1][j-1] = 3 : board[i-1][j-1] = 2;
                     }
-                })
+                    if (j + 1 < board[i].length) {
+                        board[i-1][j+1] % 2 !== 0 ? board[i-1][j+1] = 3 : board[i-1][j+1] = 2;
+                    }
+                    board[i-1][j] % 2 !== 0 ? board[i-1][j] = 3 : board[i-1][j] = 2;
+                }
+
+                if (i + 1 < board.length) {
+                    if (j - 1 >= 0) {
+                        board[i+1][j-1] % 2 !== 0 ? board[i+1][j-1] = 3 : board[i+1][j-1] = 2;
+                    }
+                    if (j + 1 < board[i].length) {
+                        board[i+1][j+1] % 2 !== 0 ? board[i+1][j+1] = 3 : board[i+1][j+1] = 2;
+                    }
+                    board[i+1][j] % 2 !== 0 ? board[i+1][j] = 3 : board[i+1][j] = 2;
+                }
+
+                if (j - 1 >= 0) {
+                        board[i][j-1] % 2 !== 0 ? board[i][j-1] = 3 : board[i][j-1] = 2;
+                    }
+                if (j + 1 < board[i].length) {
+                        board[i][j+1] % 2 !== 0 ? board[i][j+1] = 3 : board[i][j+1] = 2;
+                    }
             }
         }
     }
-    return b.length * b[0].length - bombSet.size;
+
+    for (let i = 0; i < board.length; i++) {
+        board[i].map(e => e === 0 ? count++ : e);
+    }
+
+    return count;
 }
