4d8e6446f48609f9a526cca11b921440bb6f651c
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-28 11:10:07 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,5 @@
 function solution(nums) {
-    const set = new Set(nums);
-    const setLength = [...set].length;
+    const setLength = [...new Set(nums)].length;
     const numsHalf = nums.length / 2
     return setLength > numsHalf ? numsHalf : setLength;
 } 
