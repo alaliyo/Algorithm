1ae72cb7ac434b8e2cda11bdb293024f307e15f9
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-28 10:38:51 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,5 +1,4 @@
 function solution(a, b) {
     const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    let date = new Date(`2016-${a}-${b}`);
-    return day[date.getDay()];
+    return day[new Date(`2016-${a}-${b}`).getDay()];
 }
