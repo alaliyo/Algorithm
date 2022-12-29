7383092fd0a7c767fb6c044bb4b7f89deedfc4c7
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-29 13:02:40 +0900

    [level 1] Title: 모의고사, Time: 1.67 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -9,15 +9,6 @@ function solution(answers) {
 
     let arr = [];
     
-    // for (let i = 1; i <= answers.length; i++) {
-    //     const anum = i % 5 === 0 ? 5 : i % 5;
-    //     const bnum = i % 8 === 0 ? 8 : i % 8;
-    //     const cnum = i % 10 === 0 ? 10 : i % 10;
-    //     answers[i-1] === a[anum-1] ? aCount++ : null;
-    //     answers[i-1] === b[bnum-1] ? bCount++ : null;
-    //     answers[i-1] === c[cnum-1] ? cCount++ : null;
-    // }
-    
     const max = Math.max(aCount, bCount, cCount);
     
     if (max === aCount) {arr.push(1)} 
