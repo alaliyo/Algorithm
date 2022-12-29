ed123bb9309e05f7a041a69a3669acba669f843d
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-29 13:01:58 +0900

    [level 1] Title: 모의고사, Time: 1.64 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -3,20 +3,20 @@ function solution(answers) {
     const b = [2, 1, 2, 3, 2, 4, 2, 5];
     const c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    let aCount = 0;
-    let bCount = 0;
-    let cCount = 0;
-    
+    let aCount = answers.filter((e, i) => e === a[i % a.length]).length;
+    let bCount = answers.filter((e, i) => e === b[i % b.length]).length;
+    let cCount = answers.filter((e, i) => e === c[i % c.length]).length;
+
     let arr = [];
     
-    for (let i = 1; i <= answers.length; i++) {
-        const anum = i % 5 === 0 ? 5 : i % 5;
-        const bnum = i % 8 === 0 ? 8 : i % 8;
-        const cnum = i % 10 === 0 ? 10 : i % 10;
-        answers[i-1] === a[anum-1] ? aCount++ : null;
-        answers[i-1] === b[bnum-1] ? bCount++ : null;
-        answers[i-1] === c[cnum-1] ? cCount++ : null;
-    }
+    // for (let i = 1; i <= answers.length; i++) {
+    //     const anum = i % 5 === 0 ? 5 : i % 5;
+    //     const bnum = i % 8 === 0 ? 8 : i % 8;
+    //     const cnum = i % 10 === 0 ? 10 : i % 10;
+    //     answers[i-1] === a[anum-1] ? aCount++ : null;
+    //     answers[i-1] === b[bnum-1] ? bCount++ : null;
+    //     answers[i-1] === c[cnum-1] ? cCount++ : null;
+    // }
     
     const max = Math.max(aCount, bCount, cCount);
     
