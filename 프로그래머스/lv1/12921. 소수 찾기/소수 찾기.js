9ed64569602c3bb6fe1dc95ab98b2c66c69ad2a6
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-29 12:05:01 +0900

    [level 1] Title: 소수 찾기, Time: 149.02 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,13 +1,12 @@
 function solution(n) {
-  let s= [...Array(n).keys()]
-  // console.log(s);
-  s[0]=0
-  for(let i=2; i<=parseInt(n**.5)+1;i++){
-    // console.log(i);
-
-    for (let j=2 ; j<=(n-i)/i+1; j++){
-      s[i*j-1]=0
+    let s = [...Array(n).keys()];
+    console.log(s);
+    s[0] = 0;
+    for(let i=2; i<=parseInt(n**.5)+1;i++){
+        console.log(i);
+        for (let j=2 ; j<=(n-i)/i+1; j++){
+            s[i*j-1]=0
+        }
     }
-  }
-  return s.filter(x=>Boolean(x)).length;
+    return s.filter(x=>Boolean(x)).length;
 }
