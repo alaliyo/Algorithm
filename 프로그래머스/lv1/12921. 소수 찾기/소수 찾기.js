0e1788a41882899c86bcb8757d686b47926a6f6c
alaliyo <103922690+alaliyo@users.noreply.github.com>
2022-12-29 12:09:21 +0900

    [level 1] Title: 소수 찾기, Time: 183.80 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,12 +1,28 @@
 function solution(n) {
-    let s = [...Array(n).keys()];
-    console.log(s);
-    s[0] = 0;
-    for(let i=2; i<=parseInt(n**.5)+1;i++){
-        console.log(i);
-        for (let j=2 ; j<=(n-i)/i+1; j++){
-            s[i*j-1]=0
+    let nums = [...Array(n+1).keys()];
+    let arr = [2];
+
+    const isPrime = (num) => {
+        if(num === 1) { 
+            return 0;
         }
+        
+        if(num % 2 === 0) { 
+            return 0;
+        }
+
+        const sqrt = parseInt(Math.sqrt(num));
+
+        for (let i = 3; i <= sqrt; i += 2) {
+            if(num % i === 0) return 0;
+        }
+        return num;
     }
-    return s.filter(x=>Boolean(x)).length;
+
+    for (let i = 0; i < nums.length; i++) {
+        const e = isPrime(nums[i]);
+        e !== 0 ? arr.push(e) : null;
+    }
+
+    return arr.length;
 }
