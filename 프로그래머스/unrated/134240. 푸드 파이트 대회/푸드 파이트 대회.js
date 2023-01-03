bff7b7cffdebf132043755198362249905044e27
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-03 10:24:20 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -10,18 +10,6 @@ function solution(food) {
             arr.push(i);
         }
     }
-    
-    arr.push(0);
-    
-    for(let i = food.length; i > 0; i--) {
-        let e = Math.floor(food[i] / 2);
-        if (e < 1) {
-            continue;
-        }
-        for(let j = 0; j < e; j++) {
-            arr.push(i);
-        }
-    }
-    
-    return arr.join('');
+
+    return arr.join('') + '0' + arr.reverse().join('');
 }
