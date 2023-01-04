20dcffdd0baef6eb0ad0fe4636a494fd6651adab
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-04 10:10:36 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,11 @@
+function solution(lottos, win_nums) {
+    let countArr = [7];
+    for(let i = 0; i < lottos.length; i++) {
+        lottos.map(e => e === win_nums[i] ? countArr[0]-- : e);
+    }
+    countArr.push(countArr[0]);
+    
+    lottos.map(e => e === 0 ? countArr[0]-- : e);
+    
+    return countArr.map((e, i) => e === 7 ? countArr[i] = 6 : e);
+}
