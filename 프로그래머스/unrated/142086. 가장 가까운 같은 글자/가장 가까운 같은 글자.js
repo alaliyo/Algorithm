bb74ddbd21dfc27b84622356fcfac41d81db1c52
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-05 14:08:04 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 47.50 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -7,7 +7,7 @@ function solution(s) {
         const index = sArr.indexOf(e);
          if(index !== i) {
             arr.push(i - index);
-            sArr.splice(index, 1, ' ');
+            sArr.splice(index, 1, 0);
         } else {
             arr.push(-1);
         }
