dff7220911e356f1966f123fef75c8e8b6ec4b54
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-07 13:29:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.35 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,7 +2,7 @@ function solution(participant, completion) {
     participant.sort();
     completion.sort();
     
-    for(let i = 0; i < participant.length; i++) {
+    for(let i in participant) {
         if(participant[i] !== completion[i]) {
             return participant[i];
         }
