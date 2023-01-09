7047244038fa916a4ace9da471ff8513b2228e6f
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-09 11:23:17 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,9 @@
 function solution(k, m, score) {
     let count = 0;
-    let arr = [];
     score.sort((a, b) => b - a);
     for(let i = 0; i <= score.length / m - 1; i++) {
-        arr.push(score.slice(i * m, i * m + m))
+        count += score.slice(i * m, i * m + m)[m-1] * m
     }
-    
-    arr.map(e => count += e[m-1] * m);
+
     return count;
 }
