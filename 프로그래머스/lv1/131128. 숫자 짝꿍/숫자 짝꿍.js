026850514f333a5f90ab8b2a9a5daa7e936d4d2c
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-11 13:16:02 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,26 +1,22 @@
 function solution(X, Y) {
-    let arr = [];
-    let NewObj = {};
-    const obj = (arr) => arr.reduce((ac, v) => ({ ...ac, [v]: (ac[v] || 0) + 1 }), {});
-    const xObj = obj([...X]);
-    const yObj = obj([...Y]);
+    const commonNums = [];
+    const objX = {};
+    const objY = {};
 
-    for (let i in xObj) {
-        for (let j in yObj) {
-            if (i === j) {
-                NewObj[i] = Math.min(xObj[i], yObj[j]);
-            }
+    for(const el of X) {
+        objX[el] = (objX[el] || 0) + 1
+    }
+
+    for (const el of Y) {
+        if (objX[el]) {
+            commonNums.push(el);
+            objX[el]--;
         }
     }
-    
-    for (let i in NewObj) {
-        for (let j = 0; j < NewObj[i]; j++) {
-            arr.push(i);
-        }
-    }
-    
-    arr.sort((a, b) => b - a);
-    if (arr.length === 0) return "-1";
-    if (arr[0] === '0') return '0';
-    return arr.join('');
+
+    commonNums.sort((a, b) => b - a);
+
+    if (commonNums.length === 0) return "-1";
+    if (commonNums[0] === "0") return "0";
+    return commonNums.reduce((acc, cur) => acc + cur, "");
 }
