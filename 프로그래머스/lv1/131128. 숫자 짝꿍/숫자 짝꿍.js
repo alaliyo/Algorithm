192be2aad0c8ebd806fc6ace18e58b4efc594e1c
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-11 13:12:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,26 @@
+function solution(X, Y) {
+    let arr = [];
+    let NewObj = {};
+    const obj = (arr) => arr.reduce((ac, v) => ({ ...ac, [v]: (ac[v] || 0) + 1 }), {});
+    const xObj = obj([...X]);
+    const yObj = obj([...Y]);
+
+    for (let i in xObj) {
+        for (let j in yObj) {
+            if (i === j) {
+                NewObj[i] = Math.min(xObj[i], yObj[j]);
+            }
+        }
+    }
+    
+    for (let i in NewObj) {
+        for (let j = 0; j < NewObj[i]; j++) {
+            arr.push(i);
+        }
+    }
+    
+    arr.sort((a, b) => b - a);
+    if (arr.length === 0) return "-1";
+    if (arr[0] === '0') return '0';
+    return arr.join('');
+}
