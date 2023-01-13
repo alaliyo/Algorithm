5d11cb7afae3be40f817e9dfc2d6d49c8d832cdb
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-13 09:57:32 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,5 +1,5 @@
 function solution(numbers, hand) {
-    const nmuLocation = [
+    const numsPos = [
         [4, 2], [1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]
     ];
     let left = [4, 1];
@@ -8,30 +8,30 @@ function solution(numbers, hand) {
     
     for (let i = 0; i < numbers.length; i++) {
         const e = numbers[i];
-        const location = nmuLocation[e];
-        const LC = Math.abs(left[0] - location[0]) + Math.abs(left[1] - location[1]);
-        const RC = Math.abs(right[0] - location[0]) + Math.abs(right[1] - location[1]);
+        const pos = numsPos[e];
+        const LC = Math.abs(left[0] - pos[0]) + Math.abs(left[1] - pos[1]);
+        const RC = Math.abs(right[0] - pos[0]) + Math.abs(right[1] - pos[1]);
         
         if (e === 1 || e === 4 || e === 7) {
-            left = location;
+            left = pos;
             str += 'L';
         } else if (e === 3 || e === 6 || e === 9) {
-            right = location;
+            right = pos;
             str += 'R';
         } else if (hand === 'left') {
             if (LC <= RC) {
-                left = location;
+                left = pos;
                 str += 'L';
             } else {
-                right = location;
+                right = pos;
                 str += 'R';
             }
         } else if (hand === 'right') {
             if (RC <= LC) {
-                right = location;
+                right = pos;
                 str += 'R';
             } else {
-                left = location;
+                left = pos;
                 str += 'L';
             }
         }
