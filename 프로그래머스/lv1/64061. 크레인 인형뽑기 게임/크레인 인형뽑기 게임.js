d39ced56548555dbb2e596a447dc39f598646bf3
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-14 10:34:53 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.46 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,22 @@
+function solution(board, moves) {
+    let arr = [];
+    let count = 0;
+    for (let i = 0; i < moves.length; i++) {
+        let e = moves[i];
+        for (let j = 0; j < board.length; j++) {
+            let doll = board[j][e - 1];
+            if (doll !== 0) {
+                if (doll === arr[arr.length - 1]) {
+                    arr.pop();
+                    board[j][e - 1] = 0;
+                    count++;
+                } else {
+                    arr.push(doll);
+                    board[j][e - 1] = 0;
+                }
+                break;
+            }
+        }
+    }
+    return count * 2;
+}
