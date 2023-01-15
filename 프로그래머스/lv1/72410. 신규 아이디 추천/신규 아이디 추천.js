b027a3ed9ca072e8a229c3b8f7c1ff556661c8f8
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-16 00:18:59 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,20 +1,21 @@
 function solution(new_id) {
+    // 1단계
     new_id = new_id.toLowerCase();
+    // 2단계
     new_id = new_id.replace(/[^0-9a-z|.|_|-]/g, '');
-    
+    // 3단계
     new_id = new_id.replace(/[\.]{2,}/g, '.');
-    
+    // 4단계
     new_id[0] === '.' ? new_id = new_id.substring(1) : null;
-    
+    // 5단계
     new_id.length === 0 ? new_id = 'a' : null;
-    
+    // 6단계
     new_id.length > 15 ? new_id = new_id.slice(0, 15): null;
-    
     new_id[new_id.length-1] === '.' ? new_id = new_id.slice(0, -1) : null;
-    
+    // 7단계
     while (new_id.length < 3) {
         new_id.length < 3 ? new_id += new_id[new_id.length -1] : null;
     }
-    
+    // 신규 아이디 추천
     return new_id;
 }
