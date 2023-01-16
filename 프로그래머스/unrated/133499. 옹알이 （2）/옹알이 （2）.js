be1df3331766c9a288d29f6f0d649ab610e37856
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-16 11:04:55 +0900

    [unrated] Title: 옹알이 (2), Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,15 +1,10 @@
 function solution(babbling) {
     let count = 0;
-    for (let i = 0; i < babbling.length; i++) {
-        let e = babbling[i];
+    for (let e of babbling) {
+        if (e.replace(/ayaaya|yeye|woowoo|mama/, '1').replace((/[^1]/g), '') === '1') continue;
         
-        if (e.replace(/ayaaya|yeye|woowoo|mama/, '1').replace((/[^1]/g), '') === '1') {
-            continue;
-        }
-        
-        if (e.replace(/aya|ye|woo|ma/g, '').length === 0) {
-            count++
-        }
+        if (e.replace(/aya|ye|woo|ma/g, '').length === 0) count++;
     }
+    
     return count;
 }
