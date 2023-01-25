cdb8564751b165dac9a0342d3f71759f22841d86
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-25 10:38:16 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,11 +1,11 @@
 function solution(id_list, report, k) {
-    let countArr = [];
+    let reportCounts = [];
     let reportObj = {};
     let count = [];
     report = report.map(e => e.split(' '));
     
     for (let i = 0; i < id_list.length; i++) {
-        countArr.push(0);
+        reportCounts.push(0);
         reportObj[id_list[i]] = [];
         count.push(0);
     }
@@ -15,13 +15,13 @@ function solution(id_list, report, k) {
         for (let j = 0; j < id_list.length; j++) {
             if (id_list[j] === eArr[0] && !reportObj[id_list[j]].includes(eArr[1])) {
                 reportObj[id_list[j]].push(eArr[1]);
-                countArr[id_list.indexOf(eArr[1])] += 1;
+                reportCounts[id_list.indexOf(eArr[1])] += 1;
             }
         }
     }
     
-    for (let i = 0; i < countArr.length; i++) {
-        if (countArr[i] >= k) {
+    for (let i = 0; i < reportCounts.length; i++) {
+        if (reportCounts[i] >= k) {
             for (let j in reportObj) {
                 if (reportObj[j].includes(id_list[i])) {
                     count[id_list.indexOf(j)]++;
