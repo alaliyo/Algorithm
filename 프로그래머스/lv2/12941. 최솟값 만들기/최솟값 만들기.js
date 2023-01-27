3612b813f10721eb2420943cf10237c854d6c390
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-27 09:25:01 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,6 @@
 function solution(A,B){
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
-    const total = A.map((e, i) => e * B[i]);
 
-    return total.reduce((a, b) => a + b);
+    return A.reduce((a, b, i) => a  + (b * B[i]), 0);
 }
