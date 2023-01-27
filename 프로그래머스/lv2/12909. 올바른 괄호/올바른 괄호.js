1778451cf5cb673f57d05c95cfffc75bb48df583
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-27 10:50:44 +0900

    [level 2] Title: 올바른 괄호, Time: 5.31 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,9 +1,6 @@
 function solution(s){
-    if (s[0] === ')' || s[s.length-1] === '(') return false;
-    if (s.match(/\(/g).length !== s.match(/\)/g).length) return false;
-    s = s.replaceAll('()', "")
+    s = s.replaceAll('()', "");
     if (s.length === 0) return true;
     if (s[0] === ')' || s[s.length-1] === '(') return false;
-    if (s.match(/\(/g).length === s.match(/\)/g).length) return true;
-    return false;
+    return (s.match(/\(/g).length === s.match(/\)/g).length) ? true : false;
 }
