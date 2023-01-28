e379a4c397e7edbfc17bef9d43abfbf7e5e0f366
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-28 14:02:43 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.63 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,21 +1,17 @@
 function solution(today, terms, privacies) {
+    privacies = privacies.map(e => e.split('.').join('/').split(' '));
     let arr = [];
     terms = Object.fromEntries(terms.map(e => e.split(' ')));
-    privacies = privacies.map(e => e.split('.').join('/').split(' '));
     today = new Date(today.split('.').join('/'));
     
     for (let i = 0; i < privacies.length; i++) {
-        privacies[i][0] = new Date(privacies[i][0]);
         const e = privacies[i];
+        privacies[i][0] = new Date(e[0]);
         privacies[i] = new Date(e[0].setMonth(e[0].getMonth() + Number(terms[e[1]])));
         privacies[i].setDate(privacies[i].getDate() - 1);
     }
     
-    for (let i = 0; i < privacies.length; i++) {
-        if (today > privacies[i]) {
-            arr.push(i + 1);
-        }
-    }
+    privacies.map((e, i) => today > e ? arr.push(i+1) : null);
 
     return arr;
 }
