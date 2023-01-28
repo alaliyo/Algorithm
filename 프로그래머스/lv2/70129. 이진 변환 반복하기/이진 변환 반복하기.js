204672b9d4a0d422900f5b4e7f1ed23e7a0329c7
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-28 14:31:58 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.35 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,5 +1,5 @@
 function solution(s) {
-    let arr =[0, 0];
+    let arr = [0, 0];
     while (Number(s) > 1) {
         arr[1] += s.replaceAll('1', "").length;
         s = s.replaceAll('0', "").length.toString(2);
