ec48933ab977b7f7212361de2f881df85527738e
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-01-31 14:27:35 +0900

    [level 2] Title: 피보나치 수, Time: 1.63 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,13 +1,13 @@
 function solution(n) {
-    let F1 = 0;
+    let F0 = 0;
+    let F1 = 1;
     let F2 = 1;
-    let F3 = 1;
     
     for (let i = 3; i <= n; i++) {
+        F0 = F1;
         F1 = F2;
-        F2 = F3;
-        F3 = (F1 + F2) % 1234567;
+        F2 = (F0 + F1) % 1234567;
     }
     
-    return F3;
+    return F2;
 }
