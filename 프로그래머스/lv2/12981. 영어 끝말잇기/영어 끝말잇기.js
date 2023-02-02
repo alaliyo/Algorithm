137de63d181c11cf5f5724f61b4d784142d12629
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-02-02 10:35:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,14 +1,9 @@
 function solution(n, words) {
     const arr = [];
-    
-    for (let i = 1; i < words.length; i++) {
-        const befor = words[i-1];
-        const now = words[i];
-        
-        if (befor[befor.length-1] !== now[0] || words.indexOf(now) !== i) {
-            let num = i + 1;
-            arr.push(num % n === 0 ? n : num % n);
-            arr.push(Math.ceil(num / n));
+    for (let i = 1; i < words.length; i++) {        
+        if (words[i-1][words[i-1].length-1] !== words[i][0] || words.indexOf(words[i]) !== i) {
+            arr.push((i + 1) % n === 0 ? n : (i + 1) % n);
+            arr.push(Math.ceil((i + 1) / n));
             break;
         }
     }
