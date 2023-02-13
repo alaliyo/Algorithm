ff0251751ac65fe116536b2b0ca09e7a8ee9b5a9
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-02-13 10:13:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,14 +1,12 @@
 function solution(n) {
-    let count = 1; 
-    while (n !== 1) {
+    if (n === 1) return 1;
+    let count = 0; 
+    
+    while (n > 1) {
         while (n % 2 === 0) {
             n /= 2;
         }
-        if (n !== 1) {
-            n -= 1;
-        }
-        
-        if (n === 1) return count;
+        n -= 1;
         count++;
     }
     
