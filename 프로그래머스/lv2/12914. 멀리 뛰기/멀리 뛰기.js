572699431edd6f902e909d8ea6c20a7c3e713552
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-02-20 09:47:12 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -3,12 +3,12 @@ function solution(n) {
     let num1 = 1;
     let num2 = 2;
     let num3 = 0;
-    
+
     for (let i = 2; i < n; i++) {
-        num3 = (num1 + num2) % 1234567;;
+        num3 = (num1 + num2) % 1234567;
         num1 = num2;
         num2 = num3;
     }
-    
+
     return num3;
 }
