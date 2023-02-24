318c65f5d52bc6970fba330a495568e966f4304f
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-02-24 11:36:41 +0900

    [unrated] Title: 귤 고르기, Time: 56.76 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,15 +1,15 @@
 function solution(k, tangerine) {
     let count = 0;
+    let num = 0;
     const obj = {};
     
     tangerine.forEach((e) => (obj[e] = (obj[e] || 0) + 1));
     const arr = Object.values(obj).sort((a, b) => b - a);
-
-    for (const e of arr) {
+    
+    while (k > 0) {
+        k -= arr[num];
+        num++
         count++;
-        
-        if (k > e) k -= e;
-        else break;
     }
 
   return count;
