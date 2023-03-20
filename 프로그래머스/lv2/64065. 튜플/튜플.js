b6472a6a16ccbed71f1e8463a2f05bd4d79e83a2
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-03-20 11:41:59 +0900

    [level 2] Title: 튜플, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,23 @@
+function solution(s) {
+    const answerArr = [];
+    const arr = s.replaceAll('{', '').replaceAll('}', '').split(',');
+    let obj = {};
+    
+    arr.forEach((e) => {
+        obj[e] = (obj[e] || 0) + 1;
+    });
+    
+    let objSort = [];
+    
+    for (let e in obj) {
+        objSort.push([e, obj[e]]);
+    }
+    objSort.sort((a, b) => b[1] - a[1]);
+    
+    for (let e of objSort) {
+        answerArr.push(Number(e[0]));
+    }
+    
+    return answerArr;
+}
+
