109cda656811d599634bfe3e8166f5ae13533d8f
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-03-23 11:23:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 863.59 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,12 +1,12 @@
 function solution(elements) {
-    let arr = [];
-    const eTwice = [...elements, ...elements]
-    
-    for (let i = 0; i < elements.length; i++) {
-        for (let j = 0; j < elements.length; j++) {
-            arr.push([...eTwice].splice(i, j+1).reduce((a, b) => a + b));
-        }
+  const set = new Set();
+  for (let i = 1; i <= elements.length; i++) {
+    // 원순열 만들기 i개의 원소를 뒤에 추가해준다
+    const els = elements.concat(elements.slice(0, i));
+    // set 갱신
+    for (let j = 0; j < elements.length; j++) {
+      set.add(els.slice(j, j + i).reduce((a, c) => a + c, 0));
     }
-    
-    return [...new Set(arr)].length;
+  }
+  return set.size;
 }
