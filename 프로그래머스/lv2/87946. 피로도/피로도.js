e237982bd09a1cb3adf98c3171847569395280b5
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-04-05 16:57:29 +0900

    [level 2] Title: 피로도, Time: 0.51 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -0,0 +1,26 @@
+function solution(k, dungeons) {
+    let answer = 0;
+    const len = dungeons.length;
+    
+    function DFS(i, num, count) {
+        if (i === len) { 
+            if (num <= k) { 
+                count > answer ? answer = count : null;
+            }
+            return;
+        }
+        
+        for (let j = i; j < len; j++) {
+            [dungeons[i], dungeons[j]] = [dungeons[j], dungeons[i]];
+            if (num >= dungeons[i][0]) {
+                DFS(i + 1, num - dungeons[i][1], count + 1);
+            }
+            [dungeons[i], dungeons[j]] = [dungeons[j], dungeons[i]];
+        }
+        DFS(i + 1, num, count)
+    }
+    
+    DFS(0, k, 0);
+
+    return answer;
+}
