85793f6fdf9ac5bfaa2fcdc5a43958cfcbad283b
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-04-06 17:12:21 +0900

    [level 2] Title: 할인 행사, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,6 +1,5 @@
 function solution(want, number, discount) {
     let answer = 0;
-    let count = discount.length;
     const obj = {};
     const len = discount.length - want.length;
     
@@ -9,22 +8,25 @@ function solution(want, number, discount) {
     }
     
     for (let i = 0; i < len; i++) {
-        const a = Object.assign({}, obj);
+        const copyObj = Object.assign({}, obj);
         let sum = 0;
         
         for (let j = i; j < discount.length; j++) {
             sum++;
-            if (a.hasOwnProperty(discount[j])) {
-                a[discount[j]]--;
-                if (a[discount[j]] === 0) {
-                    delete a[discount[j]];
+            
+            if (copyObj.hasOwnProperty(discount[j])) {
+                copyObj[discount[j]]--;
+                
+                if (copyObj[discount[j]] === 0) {
+                    delete copyObj[discount[j]];
                 } 
             }
             
-            if (Object.keys(a).length === 0) {
+            if (Object.keys(copyObj).length === 0) {
                 if (sum <= 10) {
                     answer++;
                 }
+                
                 break;
             }
         }
