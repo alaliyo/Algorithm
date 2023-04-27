715d8eeea52044f183d2d00fa8b6da4adb5ec169
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-04-27 12:21:49 +0900

    [level 2] Title: 오픈채팅방, Time: 131.29 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -2,14 +2,14 @@ function solution(record) {
     const useObj = {};
     const answer = [];
     const newRecord = record.map(e => e.split(' '));
-    
+
     for (let i = 0; i < newRecord.length; i++) {
         const el = newRecord[i];
         if (el[0] === 'Change' || el[0] === 'Enter') {
             useObj[el[1]] = el[2];
         }
     }
-    
+
     for (let i = 0; i < newRecord.length; i++) {
         const el = newRecord[i];
         if (el[0] === 'Enter') {
@@ -18,6 +18,6 @@ function solution(record) {
             answer.push(useObj[el[1]] + '님이 나갔습니다.');
         }
     }
-    
+
     return answer;
 }
