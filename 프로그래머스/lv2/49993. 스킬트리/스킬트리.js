a6f4981cd8da53487c46260a0d2c64bb81225677
alaliyo <103922690+alaliyo@users.noreply.github.com>
2023-04-29 12:21:22 +0900

    [level 2] Title: 스킬트리, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,29 +1,14 @@
 function solution(skill, skill_trees) {
-    let answer = 0;
-    let arr = [];
-    
-    function isDescending(el) {
-        for (let i = 0; i < el.length - 1; i++) {
-            if (el[i + 1] === -1 && el[i] >= el[i + 1]) {
-                continue;
-            } else if (el[i] > el[i + 1]) {
-                return false;
-            } else if (el[i] === -1 && el[i + 1] >= 0) {
-                return false;
-            }
+    function isCorrect(n) {
+        // const test = '[' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').filter(v => !skill.includes(v)).join('') + ']*';
+        let test = skill.split('');
+        for (var i = 0; i < n.length; i++) {
+            if (!skill.includes(n[i])) continue;
+            if (n[i] === test.shift()) continue;
+            return false;
         }
         return true;
-    }
+    }    
 
-    for (let i = 0; i < skill_trees.length; i++) {
-        for (let j = 0; j < skill.length; j++) {
-            const index = skill_trees[i].indexOf(skill[j]);
-            arr.push(index);
-        }
-        
-        isDescending(arr) ? answer++ : null
-        arr = [];
-    }
-    
-    return answer;
+    return skill_trees.filter(isCorrect).length;
 }
