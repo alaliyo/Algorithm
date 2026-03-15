e9ba55ef59591abf74a1b2be8dcbb3bf466baf44
alaliyo <103922690+alaliyo@users.noreply.github.com>
2026-03-15 20:42:57 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.52 ms, Memory: 36.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ JavaScript/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,5 +1,10 @@
+const operations = {
+  w: (n) => n + 1,
+  s: (n) => n - 1,
+  d: (n) => n + 10,
+  a: (n) => n - 10,
+};
+
 function solution(n, control) {
-    const numobj = { w: 1, s: -1, d: 10, a: -10 };
-    const arr = control.split("").map(e => numobj[e]);
-    return n + arr.reduce((a, b) => a + b);
+  return [...control].reduce((prev, op) => operations[op](prev), n);
 }
