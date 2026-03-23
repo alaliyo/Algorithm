a4fe784b5018b907f43a113b66372f4242fbaaa2
alaliyo <103922690+alaliyo@users.noreply.github.com>
2026-03-23 13:21:52 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ JavaScript/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,4 +1,3 @@
 function solution(str_list, ex) {
-    const arr = str_list.map(e => e.includes(ex) ? "" : e);
-    return arr.join("");
+    return str_list.filter(e => !e.includes(ex)).join("");
 }
