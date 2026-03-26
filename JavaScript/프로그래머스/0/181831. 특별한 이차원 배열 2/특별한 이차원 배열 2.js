75b5a89dede87a3c72a5eb78fc572ba9b44ab7d7
alaliyo <103922690+alaliyo@users.noreply.github.com>
2026-03-26 13:06:30 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 26.46 ms, Memory: 36.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ JavaScript/프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,19 +1,11 @@
 function solution(arr) {
-    let answer = 1;
     const len = arr.length;
     
     for (let i = 0; i < len; i++) {
-        if (!answer) break;
-        
-        for (let j = 0; j < len; j++) {
-            if (!answer) break;
-            
-            const num1 = arr[i][j];
-            const num2 = arr[j][i];
-            
-            if (num1 !== num2) answer = 0;
+        for (let j = 0; j < len; j++) {  
+            if (arr[i][j] !== arr[j][i]) return 0;
         }
     }
     
-    return answer;
+    return 1;
 }
